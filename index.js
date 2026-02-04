bfd06e47c419252766efa892f5a07bb923fc756c
jawwad Mughal <jawad21ullah@gmail.com>
2026-02-04 23:20:46 +0500

    done

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
-// app.options("*", cors());
+app.options("*", cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
