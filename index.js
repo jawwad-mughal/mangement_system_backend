b8927d415b898e7ea474b771febfbbf64e9e8773
jawwad Mughal <jawad21ullah@gmail.com>
2026-02-06 19:40:15 +0500

    update

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ app.use("/api/transactions", transactionRoutes);
 app.use("/summary",accountSummaryRoutes)
 
 
-app.listen(port, () => {
-  console.log(`server connected localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`server connected localhost:${port}`)
+// })
+
+export default app;
